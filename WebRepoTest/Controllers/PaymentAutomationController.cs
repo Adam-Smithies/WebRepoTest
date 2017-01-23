09274afbdb65a84a1811f190ea21c9446f277beb
AdamSmithies <adamsmithies@infund.co.uk>
2017-01-23 15:07:16 +0000

    Added the payment automation functionality

---
+++ WebRepoTest/Controllers/PaymentAutomationController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebRepoTest.Controllers
+{
+    public class PaymentAutomationController : Controller
+    {
+        // GET: PaymentAutomation
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
